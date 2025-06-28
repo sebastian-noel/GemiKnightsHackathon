c671050dbc9d275ee1e27fb830e6b9565931d9c7
evans-christian2004 <evans.christian2004@gmail.com>
2025-06-28 14:30:35 -0400

    imported untyled excalidraw component

---
+++ FE-AI/src/components/ExcalidrawComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Excalidraw } from "@excalidraw/excalidraw";
+const ExcalidrawComponent = () => {
+  return (
+    
+    <div style={{ width: 800, height: 600 }} className="custom-styles">
+        <Excalidraw/>
+    </div>
+  )
+}
+
+export default ExcalidrawComponent
