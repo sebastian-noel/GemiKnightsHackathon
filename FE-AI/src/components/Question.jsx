336da1e3cd313b6ceda619bf3e9db9df170dff6b
Stevin006 <Steving2006@gmail.com>
2025-06-28 19:25:13 -0400

    ImageUploadedAndQuestionImplemented

--- FE-AI/src/components/Question.jsx
+++ FE-AI/src/components/Question.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Zoom from 'react-medium-image-zoom'
 
 const Question = () => {
   return (
-    <div className="question">Q</div>
+    <div >
+    <img
+      alt="That Wanaka Tree, New Zealand by Laura Smetsers"
+      src="src/assets/FeData/FE-Aug22-03.jpg"
+      className = "QuestionViewer"
+    />
+  </div>
   )
 }
 
